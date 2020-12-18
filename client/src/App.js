c23250a044ae9be1b7f3a9d5e7e906ad97413028
DenizAri <58032174+DenizAri@users.noreply.github.com>
2020-12-18 15:29:16 +0000

    Deniz/Carousel
    LanguageCarousel.js and LanguageCarousel.scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Carousel from "./Carousel/LanguageCarousel";
+// import Carousel from "react-bootstrap/Carousel";
+import "./App.scss";
 
-import "./App.css";
-import { getMessage } from "./service";
-import logo from "./logo.svg";
+
 
 export function App() {
-	const [message, setMessage] = useState("Loading...");
 
-	useEffect(() => {
-		getMessage().then((message) => setMessage(message));
-	}, []);
 
 	return (
 		<main role="main">
-			<div>
-				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
-				<h1 className="message" data-qa="message">{message}</h1>
-			</div>
+
+			 <Carousel />
 		</main>
 	);
 }
--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -14,8 +14,14 @@ module.exports = {
 				loader: "file-loader",
 			},
 			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader", // Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
 			},
 		],
 	},
