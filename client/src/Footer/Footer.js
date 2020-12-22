ce8179d0ba009d8ddf720cad868875aecd8765a5
edgar <edgarendriulis@gmail.com>
2020-12-22 19:17:48 +0000

    Completed the Header and Footer, still needs styling completion

--- client/src/Footer/Footer.js
+++ client/src/Footer/Footer.js
@@ -5,6 +5,24 @@ import "./Footer.scss";
 const Footer = () => {
     return (
         <footer className="footer">
+          <div id="communityLinks">
+            <span>Our community</span>
+          <ul>
+              <li><a href="#">Website</a></li>
+              <li><a href="#">Documentation</a></li>
+              <li><a href="#">GitHub</a></li>
+            </ul>
+          </div>
+          
+          <div id="socialLinks">
+            <span>Socials</span>
+            <ul>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Instagram</a></li>
+            </ul>
+          </div>
+          
         <div className="registration">
           © All rigths reserved | Registered charity 1174929 |{' '}
           <a
