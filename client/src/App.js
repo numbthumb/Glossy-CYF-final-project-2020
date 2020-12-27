22d8f480c3c36e8ad0da1f8bf6558b378656f2a0
edgar <edgarendriulis@gmail.com>
2020-12-27 11:44:22 +0000

    Saved minor changes so that I can pull new changes when switching branch to master and commited them

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,25 @@ import Header from "./Header/Header.js";
 import Footer from "./Footer/Footer.js";
 import "./App.scss";
 import MainBody from "./Main_Body/MainBody.js";
-import Carousel from "./Carousel/LanguageCarousel";
-// import Carousel from "react-bootstrap/Carousel";
-import { getMessage } from "./service";
-import logo from "./logo.svg";
-import SidebarNav from "./components/SidebarNav";
+// import Carousel from "./Carousel/LanguageCarousel";
+// // import Carousel from "react-bootstrap/Carousel";
+// import { getMessage } from "./service";
+// import logo from "./logo.svg";
+// import SidebarNav from "./components/SidebarNav";
+import AddTermUser from "./Add-term-user/AddTermUser.js";
 
 
 export function App() {
   return (
     <React.Fragment>
       <Header />
-      <main role="main">
+      <AddTermUser />
+      {/* <main role="main">
       <div className="App">
 			  <Carousel />
         <SidebarNav />
 			</div>
-		</main>
+		</main> */}
 	  <MainBody />
 	  <Footer />
     </React.Fragment>
--- client/src/components/SidebarNav.js
+++ client/src/components/SidebarNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "react-sidebar";
-import "../App.css";
+import "./App.scss";
 import {SidebarData} from "./SidebarData";
 
 const SidebarNav = () => {
