fae44f01e9f015c85f6e23468867267544321bb4
Amanul Islam <amanulislamnaheem@gmail.com>
2021-01-06 20:16:17 +0000

    I've added Add term button which contains a Add term form

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,44 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 import Header from "./Header/Header.js";
 import Footer from "./Footer/Footer.js";
 import "./App.scss";
-import MainBody from "./Main_Body/MainBody.js";
-// import Carousel from "./Carousel/LanguageCarousel";
-// // import Carousel from "react-bootstrap/Carousel";
+import Carousel from "./Home/Carousel/LanguageCarousel";
+import SearchBar from "./Home/Searchbar/Searchbar.js";
 // import { getMessage } from "./service";
 // import logo from "./logo.svg";
-// import SidebarNav from "./components/SidebarNav";
-import AddTermUser from "./Add-term-user/AddTermUser.js";
+// import SidebarNav from "./TermPage/SidebarNav/SidebarNav.js";
+import AddTermUserBtn from "./TermPage/AddTermUserBtn.js";
 
 
 export function App() {
   return (
     <React.Fragment>
-      <Header />
-      <AddTermUser />
-      {/* <main role="main">
-      <div className="App">
-			  <Carousel />
-        <SidebarNav />
-			</div>
-		</main> */}
-	  <MainBody />
-	  <Footer />
+      <Router>
+        <Header />
+        <Container>
+          <Switch>
+            <Route exact path="/">
+              <Link to="/TermPage">
+                <SearchBar />
+                <Carousel />
+              </Link>
+            </Route>
+            <Route exact path="/TermPage">
+              <AddTermUserBtn />
+              {/* <SidebarNav /> */}
+            </Route>
+            <Route>
+              <Link to="/LoginPage">
+                <BackToHome />
+                <LoginForm />
+              </Link>
+            </Route>
+          </Switch>
+        </Container>
+        <Footer />
+      </Router>
     </React.Fragment>
   );
 }
