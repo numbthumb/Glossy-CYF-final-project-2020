c2565693495da59569c7080984dda1cb0a11cbbf
edgar <edgarendriulis@gmail.com>
2021-01-15 23:10:55 +0000

    Added the submit functionality
    The submit button has the functionality to accept the input fields.
    Gives a submit message and the close window button closes the form

--- client/src/TermPage/AddTermUserForm.js
+++ client/src/TermPage/AddTermUserForm.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form, Col, Container, FormLabel, Button } from "react-bootstrap";
 import "./AddTermUserForm.scss";
 
-const AddTermUserForm = () => {
+
+const AddTermUserForm = ({handleClose}) => {
+const [submitted, setSubmitted] = useState(false)
+const handleClick = () => setSubmitted(true)
+// const [closeForm, setCloseForm] = useState(false)
+// const handleClose = () => {setCloseForm(!closeForm); console.log(closeForm)}
+console.log(handleClose);
+
   return (
       <Container className="AddTermBox">
-        <Button variant="primary">x</Button>{" "}
+        <div className="closeForm">
+        <Button variant="primary" className="closeFormBtn" onClick={handleClose}>x</Button>{" "}</div> 
+          {submitted ? <p className="submitMessage">Your term has been submitted to Code Your future admin, please await a response from admin.<br></br> Your response from Code Your Future admin will be sent to your email.<br></br> Thank you.</p> :
+        <>
         <Form className="AddTermForm">
           <Form.Row>
             <Col>
               <FormLabel className="FormLabel">Name:*</FormLabel>
               <Form.Control
                 as="input"
-                size="lg"
                 placeholder="Name"
                 className="FormInput"
+                required
               />
             </Col>
             <Col>
@@ -23,6 +33,7 @@ const AddTermUserForm = () => {
                 as="input"
                 placeholder="Email"
                 className="FormInput"
+                required
               />
             </Col>
           </Form.Row>
@@ -33,6 +44,7 @@ const AddTermUserForm = () => {
                 as="input"
                 placeholder="Add terminology"
                 className="FormInput"
+                required
               />
             </Col>
             <Col>
@@ -41,6 +53,7 @@ const AddTermUserForm = () => {
                 as="textarea"
                 placeholder="Definition"
                 className="FormInputDefinition"
+                required
               />
             </Col>
             <Col>
@@ -49,6 +62,7 @@ const AddTermUserForm = () => {
                 as="input"
                 placeholder="Video link"
                 className="FormInput"
+                required
               />
             </Col>
             <Col>
@@ -57,13 +71,17 @@ const AddTermUserForm = () => {
                 as="input"
                 placeholder="Website link"
                 className="FormInput"
+                required
               />
             </Col>
           </Form.Row>
         </Form>
-        <Button variant="primary" className="SubmitBtn">
+        <div className="submit">
+        <Button variant="primary" className="SubmitBtn" onClick={handleClick}>
           Submit
-        </Button>{" "}
+        </Button>{" "}</div>
+        </>
+        }
       </Container>
   );
 };
